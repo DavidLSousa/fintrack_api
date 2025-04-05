c04fff14c048ade64a77f1d910ed4c23674dc29c
David Sousa <davidlsousa.dev@gmail.com>
2025-04-05 18:19:58 -0300

    implementing prost create ticker

--- src/main/java/dev/davidlsousa/fintrack_api/controllers/TickerController.java
+++ src/main/java/dev/davidlsousa/fintrack_api/controllers/TickerController.java
@@ -1,13 +1,13 @@
 package dev.davidlsousa.fintrack_api.controllers;
 
-import dev.davidlsousa.fintrack_api.Model.Ticker;
+import dev.davidlsousa.fintrack_api.Model.tickers.Ticker;
+import dev.davidlsousa.fintrack_api.Model.tickers.TickerRequestDTO;
 import dev.davidlsousa.fintrack_api.repositories.TickerRepository;
+import dev.davidlsousa.fintrack_api.services.tickerInfo.TickerInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,10 +17,38 @@ public class TickerController {
 
     @Autowired
     TickerRepository repository;
+    @Autowired
+    TickerInfoService tickerInfoService;
 
     @GetMapping
     public ResponseEntity<List<Ticker>> getAllTickers() {
         List<Ticker> listTicker = repository.findAll();
         return ResponseEntity.status(HttpStatus.OK).body(listTicker);
+    }
+
+    @PostMapping
+    public ResponseEntity<String> createTicker(@RequestBody TickerRequestDTO tickerRequestDTO) {
+
+//        Ticker ticker = tickerInfoService.getInfoTicker(tickerRequestDTO.getTicker());
+//
+//        if (ticker == null) {
+//            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Ticker not found");
+//        }
+
+        double price = tickerRequestDTO.getTotalValuePurchased() / tickerRequestDTO.getNumberOfTicker();
+
+        Ticker ticker = new Ticker(
+                tickerRequestDTO.getTicker(),
+                tickerRequestDTO.getTicker(),
+                tickerRequestDTO.getNumberOfTicker(),
+                tickerRequestDTO.getTotalValuePurchased(),
+                price,
+                price,
+                price
+        );
+
+        repository.save(ticker);
+
+        return ResponseEntity.status(HttpStatus.OK).body("Ticker added");
     }
 }
