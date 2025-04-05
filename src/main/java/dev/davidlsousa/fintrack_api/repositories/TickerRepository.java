0532d9049a11951514f4bc7e1270e11eb209daa0
David Sousa <davidlsousa.dev@gmail.com>
2025-04-05 18:06:09 -0300

    updating import model for repository

--- src/main/java/dev/davidlsousa/fintrack_api/repositories/TickerRepository.java
+++ src/main/java/dev/davidlsousa/fintrack_api/repositories/TickerRepository.java
@@ -1,10 +1,8 @@
 package dev.davidlsousa.fintrack_api.repositories;
 
-import dev.davidlsousa.fintrack_api.Model.Ticker;
+import dev.davidlsousa.fintrack_api.Model.tickers.Ticker;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.UUID;
 
 @Repository
 public interface TickerRepository extends JpaRepository<Ticker, String> {
