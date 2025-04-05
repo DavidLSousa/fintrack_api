4d3b3a27583a1966fc92fab9e222055c4cc6bf77
David Sousa <davidlsousa.dev@gmail.com>
2025-04-05 18:06:56 -0300

    fix: updating "tickit" -> "ticker"

--- src/main/resources/db/migration/V2__rename_table.sql
+++ src/main/resources/db/migration/V2__rename_table.sql
@@ -2,9 +2,9 @@ DROP TABLE tickets;
 
 CREATE TABLE tickers (
     id UUID PRIMARY KEY,
-    name_ticket VARCHAR(255) NOT NULL,
-    ticket VARCHAR(50) UNIQUE NOT NULL,
-    number_of_tickets INT NOT NULL CHECK (number_of_tickets > 0),
+    name_ticker VARCHAR(255) NOT NULL,
+    ticker VARCHAR(50) UNIQUE NOT NULL,
+    number_of_tickers INT NOT NULL CHECK (number_of_tickers > 0),
     total_value_purchased DECIMAL(10,2) NOT NULL CHECK (total_value_purchased > 0),
     highest_price DECIMAL(10,2) NOT NULL CHECK (highest_price > 0),
     lowest_price DECIMAL(10,2) NOT NULL CHECK (lowest_price > 0),
